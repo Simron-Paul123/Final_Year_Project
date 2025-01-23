ed51290e413e251504dbd78c758d3010c5f82ac2
Simron Paul <122999995+Simron-Paul123@users.noreply.github.com>
2025-01-23 14:18:57 +0000

    updated

--- SkillMingle/skill/resume_parsing.py
+++ SkillMingle/skill/resume_parsing.py
@@ -1,13 +1,13 @@
-from .models import Job
-from pdfminer.converter import TextConverter
-from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
-from pdfminer.layout import LAParams
-from pdfminer.pdfpage import PDFPage
-from pdfminer.high_level import extract_text
+#from .models import Job
+#from pdfminer.converter import TextConverter
+#from pdfminer.pdfinterp import PDFPageInterpreter, PDFResourceManager
+#from pdfminer.layout import LAParams
+#from pdfminer.pdfpage import PDFPage
+#from pdfminer.high_level import extract_text
 import io
-import pandas as pd
-import spacy
-from spacy.matcher import Matcher
+#import pandas as pd
+#import spacy
+#from spacy.matcher import Matcher
 import re
 
 nlp = spacy.load('en_core_web_sm')
